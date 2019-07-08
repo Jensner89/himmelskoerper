c6b4309842fbe1be5f4c75425514253c1d6d44f1
Jensner89 <47473170+Jensner89@users.noreply.github.com>
2019-07-08 22:59:49 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,20 +1,20 @@
 window.onload = function() {
 
     var content = {
-        firstName: 'Nicole',
-        lastName: 'Meyer',
-        street: 'Hindenburgallee 265d',
-        city: '29633 Münster',
+        firstName: 'Simone',
+        lastName: 'Kaufmann',
+        street: 'Landstraße 22b',
+        city: '30890 Barsinghausen',
         phone: '0251 / 1204193152',
         mail: 'info@himmelskörper-wellness.de',
-        birthday: '17.03.1982',
-        birth_location: 'Stuttgart',
+        birthday: '09.07.1983',
+        birth_location: 'Langenhagen',
         ust_id: 'DE1234567890',
         wirtschaftId: '',
         twitter: 'https://www.twitter.com',
         facebook: 'https://facebook.com',
         linkedIn: 'https://www.linkedin.com',
-        aufsichtBehoerde: 'Stuttgart'
+        aufsichtBehoerde: 'Hannover'
     };
 
 
